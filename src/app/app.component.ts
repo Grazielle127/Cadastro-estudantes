5e965549c9e991f33494c032fc091d590c8962c2
Grazielle Nascimento Ferreira <90863111+Grazielle127@users.noreply.github.com>
2022-05-30 18:57:08 -0300

    student

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Alunas de Excelência em 2022';
+}
---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,10 @@
+import { Produto } from "./produto";
+
+export const PRODUTOS: Produto[] = [
+  { id: 1, name: 'Lais Sanches', preco: 10, qtdEstoque: 1, categoria: 'Ingles'},
+  { id: 2, name: 'Ana Laura ', preco: 10, qtdEstoque: 4, categoria: 'Filosofia'},
+  { id: 3, name: 'Melissa Blossom  ', preco: 10, qtdEstoque: 2, categoria: 'Fisica'},
+  { id: 4, name: 'Britney falls', preco: 10, qtdEstoque: 3, categoria: 'Literatura' },
+  { id: 5, name: 'Emillie Gons', preco: 10, qtdEstoque: 5, categoria: 'História' },
+ 
+];
